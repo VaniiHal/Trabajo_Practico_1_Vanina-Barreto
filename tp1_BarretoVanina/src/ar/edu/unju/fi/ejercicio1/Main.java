439baa12279fad74251f744d865a1f8aa5b4c2bb
Vanina <VaniiHal@gmail>
2024-04-04 15:31:17 -0300

    Ejercicio 1 resuleto - Barreto Halblaub Vanina

--- tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,7 +3,16 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Bienvenidos soy Vanina Barreto Halblaub");
+		int num1 = 2;
+		int num2 = 5;
+		int num3 = 10;
+		int num4 = 8;
+		int num5 = 9;
+		float prom;
+		float suma = num1+num2+num3+num4+num5;
+		prom = suma/5;
+		System.out.println(num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " + num5);
+		System.out.println("El promedio es: " + prom);
 	}
 
 }
