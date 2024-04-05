f8fe58902560c5ce2296f49bb681ea36a909b6ba
VaniiHal <VaniiHal@gmail>
2024-04-04 21:48:18 -0300

    Ejercicio 4 resuelto

---
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -0,0 +1,41 @@
+package ar.edu.unju.fi.ejercicio4;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int numero=0, factorial=0;
+		numero = ingresarValor(numero);
+		factorial = calcularFactorial(numero);
+		System.out.println("El factorial de " + numero + " es: " + factorial);
+	}
+	
+	public static int ingresarValor(int num) {
+		Scanner entrada = new Scanner(System.in);
+		do {
+			System.out.println("Ingrese un numero entre 0 y 10");
+			while(entrada.hasNextInt() == false) {
+				System.out.println("El valor ingresado no es un numero");
+				entrada.next();
+			}
+			num=entrada.nextInt();
+			if (num<0 || num>10) {
+				System.out.println("El valor no se encuentra en el rango");
+				}
+		}while(num<0 || num>10);
+		entrada.close();
+		return num;
+	}
+	
+	public static int calcularFactorial(int num) {
+		int fact=1, cont=1;
+		while(cont<=num) {
+			fact=fact*cont;
+			cont++;
+		}
+		return fact;
+	}
+
+}
---
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -0,0 +1,35 @@
+package ar.edu.unju.fi.ejercicio5;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int numero=0;
+		numero=ingresarValor(numero);
+		multiplicarNumero(numero);
+	}
+	
+	public static int ingresarValor(int num) {
+		Scanner entrada = new Scanner(System.in);
+		do {
+			System.out.println("Ingrese un numero entre 1 y 9");
+			while(entrada.hasNextInt() == false) {
+				System.out.println("El valor ingresado no es un numero");
+				entrada.next();
+			}
+			num=entrada.nextInt();
+			if (num<1 || num>9) {
+				System.out.println("El valor no se encuentra en el rango");
+				}
+		}while(num<1 || num>9);
+		entrada.close();
+		return num;
+	}
+	
+	public static void multiplicarNumero(int num) {
+		for (int k=0;k<=10;k++) {
+			System.out.println(num + " x " + k + " = " + num*k);
+		}
+	}
+}
