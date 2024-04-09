07c776f5525176db60dfe386fc14ff62d7848587
VaniiHal <VaniiHal@gmail>
2024-04-09 20:46:07 -0300

    Ejercicio 8 resuelto

---
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio8/Main/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio8.Main;
+
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio8.Model.CalculadoraEspecial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Ingrese un numero:");
+		int num = entrada.nextInt();
+		CalculadoraEspecial calculadora = new CalculadoraEspecial();
+		calculadora.setN(num);
+		System.out.println("\nEl resultado de la sumatoria es: " + calculadora.calcularSumatoria() +"\nEl resultado de la productoria es: " + calculadora.calcularProductoria());
+		entrada.close();
+		
+		
+	}
+
+}
