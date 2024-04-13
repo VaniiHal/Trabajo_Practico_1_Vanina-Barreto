0c5c65cd86645333fe60b9d1657e70bc51cd925b
VaniiHal <VaniiHal@gmail>
2024-04-13 20:58:22 -0300

    Ejercicio 11 resuelto

---
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio11/Main/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio11.Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int k=2;
+		for (int j=40; j>0; j--) {
+			System.out.print((j*k) + "  ");
+			k++;
+		}
+	}
+
+}
