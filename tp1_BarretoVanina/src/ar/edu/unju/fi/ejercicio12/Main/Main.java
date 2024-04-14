59f651d164cde31e634c3e556ef7753b4ea24c9e
VaniiHal <VaniiHal@gmail>
2024-04-14 16:56:54 -0300

    Paquete Ejercicio 12

---
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio12/Main/Main.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.ejercicio12.Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio12/Model/Persona.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.fi.ejercicio12.Model;
+
+public class Persona {
+
+}
