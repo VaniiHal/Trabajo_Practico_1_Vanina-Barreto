55162475109480ee08f3485ce14b6639ef05ab66
Vanina <VaniiHal@gmail>
2024-04-16 15:42:42 -0300

    EJERCICIO 11 ES PROCESO

--- tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio12/Model/Persona.java
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio12/Model/Persona.java
@@ -1,5 +1,47 @@
 package ar.edu.unju.fi.ejercicio12.Model;
 
-public class Persona {
+import java.time.LocalDate;
 
+public class Persona {
+	private String nombre;
+	private LocalDate fechaNac;
+	
+	
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", fechaNac=" + fechaNac + "]";
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public LocalDate getFechaNac() {
+		return fechaNac;
+	}
+	
+	public void setFechaNac(LocalDate fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+	
+	public int calcularEdad()
+	{
+		return 0;
+	}
+	
+	public String SignoZodiacal() {
+		String signo="HOLA"; //sacar iniciliazacion
+		return signo;
+	}
+	
+	public String EstacionAnual() {
+		String estacion="HOLA";//sacar iniciliazacion
+		return estacion;
+	}
 }
+
+
