90ac6a2da184f2cafea55477f338bcbdc19d536f
VaniiHal <VaniiHal@gmail>
2024-04-17 14:50:48 -0300

    EJERCICIO 15 RESUELTO

--- tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio15/Main.java
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio15/Main.java
@@ -12,12 +12,18 @@ public class Main {
 			num=entrada.nextInt();
 		}
 		String nombres[] = new String[num];
+		System.out.print("\n");
 		for(int k=0;k<num;k++) {
 			System.out.print("Ingrese un nombre de persona: ");
 			nombres[k]=entrada.next();
 		}
+		System.out.print("\n");
 		for(int k=0;k<num;k++) {
-			System.out.println("Indice: " + k + "\tValor del indice: " + nombres[k]);
+			System.out.print(nombres[k] + "\t");
+		}
+		System.out.println("\n");
+		for(int j=num-1;j>=0;j--) {
+			System.out.print(nombres[j] + "\t");
 		}
 		entrada.close();
 	}
