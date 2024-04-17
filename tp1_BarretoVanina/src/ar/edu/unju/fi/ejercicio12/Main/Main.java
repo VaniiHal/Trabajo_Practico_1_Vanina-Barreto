07fdaaebbb9aaa7a5c3b144e897f0a971a8c7073
Vanina <VaniiHal@gmail>
2024-04-17 12:11:36 -0300

    EJERCICIIO 12 RESUELTO

--- tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio12/Main/Main.java
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio12/Main/Main.java
@@ -1,9 +1,37 @@
 package ar.edu.unju.fi.ejercicio12.Main;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+import ar.edu.unju.fi.ejercicio12.Model.Persona;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Persona persona = new Persona();
+		Scanner entrada = new Scanner(System.in);
+		LocalDate fechaNac =null;
+		System.out.print("Ingrese nombre: ");
+		String nombre = entrada.nextLine();
+		while (fechaNac == null) {
+			System.out.print("Ingrese fecha de Nacimiento (Formato dd-mm-yyyy): ");
+			String fecha = entrada.nextLine();
+			try {
+				DateTimeFormatter formato= DateTimeFormatter.ofPattern("dd/MM/yyyy");
+				fechaNac = LocalDate.parse(fecha,formato);
+			}catch(DateTimeParseException e) {
+				System.out.println("\nError en el ingreso de la fecha de nacimeinto. Intentalo de nuevo\n");
+			}
+		}
 		
+		persona.setFechaNac(fechaNac);
+		persona.setNombre(nombre);
+		int edad =persona.calcularEdad();
+		String signo = persona.ObtenerSignoZodiacal();
+		String estacion = persona.ObtenerEstacionAnual();
+		System.out.println("\n" + persona + "\nEdad: " + edad + " años\nSigno del zodiaco: "  + signo + "\nEstación: " + estacion);
+		entrada.close();
 	}
 
 }
--- tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio12/Model/Persona.java
+++ tp1_BarretoVanina/src/ar/edu/unju/fi/ejercicio12/Model/Persona.java
@@ -1,6 +1,8 @@
 package ar.edu.unju.fi.ejercicio12.Model;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class Persona {
 	private String nombre;
@@ -9,7 +11,8 @@ public class Persona {
 	
 	@Override
 	public String toString() {
-		return "Persona [nombre=" + nombre + ", fechaNac=" + fechaNac + "]";
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		return "Nombre: " + nombre + "\nFecha de nacimiento: " + fechaNac.format(formato);
 	}
 	
 	public String getNombre() {
@@ -30,16 +33,70 @@ public class Persona {
 	
 	public int calcularEdad()
 	{
-		return 0;
+		LocalDate hoy =LocalDate.now();
+		int edad = Period.between(fechaNac, hoy).getYears();
+		return edad;
 	}
 	
-	public String SignoZodiacal() {
-		String signo="HOLA"; //sacar iniciliazacion
+	public String ObtenerSignoZodiacal() {
+		String signo="";
+		int dia=fechaNac.getDayOfMonth();
+		int mes=fechaNac.getMonthValue();
+		if ((mes == 1 && dia >= 20) || (mes == 2 && dia <= 18)){
+			signo="Acuario";
+		}
+		else if ((mes == 2 && dia >= 19) || (mes == 3 && dia <= 20)) {
+			signo="Piscis";
+		}
+		else if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 19)) {
+			signo="Aries";
+		}
+		else if ((mes == 4 && dia >= 20) || (mes == 5 && dia <= 20)) {
+			signo="Tauro";
+		}
+		else if ((mes == 5 && dia >= 21) || (mes == 6 && dia <= 20)) {
+			signo="Geminis";
+		}
+		else if ((mes == 6 && dia >= 21) || (mes == 7 && dia <= 22)) {
+			signo="Cancer";
+		}
+		else if ((mes == 7 && dia >= 23) || (mes == 8 && dia <= 22)) {
+			signo="Leo";
+		}
+		else if ((mes == 8 && dia >= 23) || (mes == 9 && dia <= 22)) {
+			signo="Virgo";
+		}
+		else if ((mes == 9 && dia >= 23) || (mes == 10 && dia <= 22)) {
+			signo="Libra";
+		}
+		else if ((mes == 10 && dia >= 23) || (mes == 11 && dia <= 21)) {
+			signo="Escorpio";
+		}
+		else if ((mes == 11 && dia >= 22) || (mes == 12 && dia <= 21)) {
+			signo="Sagitario";
+		}
+		else if ((mes == 12 && dia >= 22) || (mes == 1 && dia <= 19)) {
+			signo="Capricornio";
+		}
 		return signo;
 	}
 	
-	public String EstacionAnual() {
-		String estacion="HOLA";//sacar iniciliazacion
+	public String ObtenerEstacionAnual() {
+		String estacion="";
+		int dia=fechaNac.getDayOfMonth();
+		int mes=fechaNac.getMonthValue();
+		if ((mes == 12 && dia >= 21) || (mes >= 1 && mes <= 2) || (mes==3 && dia<=20)) {
+			estacion="Verano";
+		}
+		else if((mes == 3 && dia >= 21) || (mes >= 4 && mes <= 5) || (mes==6 && dia<=20)) {
+			estacion="Otoño";
+		}
+		else if((mes == 6 && dia >= 21) || (mes >= 7 && mes <= 8) || (mes==9 && dia<=20)) {
+			estacion="Invierno";
+		}
+		else {
+			estacion="Primavera";			
+		}
 		return estacion;
 	}
 }
